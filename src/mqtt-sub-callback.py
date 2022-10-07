8f33d8afeecaeac6fe1bbc34e1d303977c141bcd
shuntaka9576 <shuntaka9576@gmail.com>
2022-10-08 07:46:15 +0900

    add subscribe callback sample

---
+++ src/mqtt-sub-callback.py
@@ -0,0 +1,14 @@
+
+
+def createMQTTConnection(device_certificate_filepath: str,private_key_filepath: str,
+        ca_certificate_filepath: str,
+        client_id: str,):
+
+        print("create")
+
+
+def main():
+    print("start")
+
+if __name__ == "__main__":
+    main()
